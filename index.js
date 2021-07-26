6a933cf6f59b98647f7f62efc51d02d216644d70
Billy <yangxiaohu.cao@griffithuni.edu.au>
2021-07-26 18:27:13 +1000

    adding files

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("Hello World, Node.js!")
+
+
+const arrayAdd = require('array-add-num');
+ 
+let x = arrayAdd([5,4,3,8]);
+
+console.log("Sum of Array is",x)
+
+console.log("array-add-num:", "^1.0.2")
