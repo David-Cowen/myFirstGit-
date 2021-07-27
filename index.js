a3a0a7edb055df368af9b26411a6adf62c542217
David Cowen <55639485+David-Cowen@users.noreply.github.com>
2021-07-27 20:22:47 +1000

    partner changes

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
+console.log("I caused a merge conflict.")
 
-console.log("Hello World, Node.js!")
 
-function adding(a,b){
-  let sum = a+b
-  console.log(sum)
-}
-
-adding(5,10)
-// const arrayAdd = require('array-add-num');
+const arrayAdd = require('array-add-num');
  
-// let x = arrayAdd([5,4,3,8]);
+let x = arrayAdd([5,4,3,8]);
 
-// console.log("Sum of Array is",x)
+console.log("Sum of Array is",x)
 
-// console.log("array-add-num:", "^1.0.2")
+console.log("array-add-num:", "^1.0.2")
